773818bffc6e7c633ed712ad2e9666e138b72959
DaBu Games <buitelaardaan2004@gmail.com>
2026-01-11 11:16:32 +0100

    jumpad + 4th section
    made it so the jumpad always launches the player instead of only launching on the beat. Because the timing was a little to difficult to do.  I also added a 4th section where the finish is for now.

--- JamParkour/Assets/Scripts/JumpPad.cs
+++ JamParkour/Assets/Scripts/JumpPad.cs
@@ -5,43 +5,17 @@ public class JumpPad : MonoBehaviour
 {
     [SerializeField] private float launchForce = 20f;
     
-    private PlayerController player;
-    private int lastBeat = -1;
-    
     public void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent(out PlayerController pc))
         {
-            player = pc;
+            LaunchPlayer(pc);
         }
     }
 
-    public void OnTriggerExit(Collider other)
-    {
-        if (other.TryGetComponent(out PlayerController pc) && pc == player)
-        {
-            player = null;
-        }
-    }
-
-    void Update()
-    {
-        if (FMODMusicEvents.inBeatWindow && player)
-        {
-            int currentBeat = FMODMusicEvents.currentBeat;
-
-            if (currentBeat != lastBeat)
-            {
-                lastBeat = currentBeat;
-                LaunchPlayer();
-            }
-        }
-    }
-
-    private void LaunchPlayer()
+    private void LaunchPlayer(PlayerController player)
     {
         Vector3 launchDir = transform.up;
-
         player.Launch(launchDir, launchForce);
     }
 }
